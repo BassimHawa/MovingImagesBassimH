e90d3add26d3a088f3ae6d4de2135a2d7560633e
Bassim Hawa <s436437@ocsb.local>
2018-03-06 11:01:27 -0500

    please work this time

--- main.lua
+++ main.lua
@@ -12,6 +12,7 @@ display.setStatusBar(display.HiddenStatusBar)
 --global variables
 scrollSpeed = 3
 scrollSpeed2 = -4
+scrollSpeed3 = 2
 
 --background image with width and height
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
@@ -21,6 +22,9 @@ local beetleship = display.newImageRect("Images/beetleship.png", 100, 100)
 
 --character image with width and height
 local gumby = display.newImageRect("Images/Gumby.png", 125, 125)
+
+-- character image with width and height
+local hotdog = display.newImageRect("Images/hotdog2.png", 100, 100)
 
 --make the beetleship transparent
 beetleship.alpha = 0
@@ -32,6 +36,10 @@ beetleship.y = display.contentHeight/3
 --set the initial x and y position of gumby
 gumby.x = 400
 gumby.y = display.contentHeight*2.5/3
+
+--set the initial x and y position of the beetleship
+hotdog.x = 100
+hotdog.y = 10
 
 --Function: MoveShip
 --Input: this function accepts an event listener
@@ -56,9 +64,27 @@ local function MoveGumby(event)
 	  gumby.x = gumby.x + scrollSpeed2
 	  -- change the transparency of gumby every time he moves so that he fades out
 	  gumby.alpha = gumby.alpha - 0.01
+	  --make gumby smaller as he moves
+	  gumby:scale(1 - 0.01, 1 - 0.01)
 	end
 
--- MoveShip will be called over and over again
+-- MoveGumby will be called over and over again
 Runtime:addEventListener("enterFrame", MoveGumby)
 
 
+--Function: MoveHotdog
+--Input: this function accepts an event listener
+--Output: none
+--Description: This function adds the scroll speed to the x-value of the hotdog
+local function MoveHotdog(event)
+	  --add the scroll speed to the x-value of the hotdog
+	  hotdog.x = hotdog.x + scrollSpeed3
+	  hotdog.y = hotdog.y + scrollSpeed3
+	  -- make the hotdog spin as it moves
+	  hotdog:rotate(20)
+	  --make the hotdog larger as it moves
+	  hotdog:scale(1 + 0.005, 1 + 0.005 ) 
+	end
+
+-- MoveHotdog will be called over and over again
+Runtime:addEventListener("enterFrame", MoveHotdog)
